ca13bece6146e6919f65337cc15dc3a087740fab
Drew Hutton Yoroshi <drew.hutton@pm.me>
2021-12-25 16:40:35 +1030

    feat: update links, fix readme

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,4 +1,7 @@
 <div class="text-sm text-center w-full h-8">
-  Created with curiosity by Drew Hutton <a class="text-purple-400 hover:text-purple-600" href="/">Yoroshikun</a>. API by
+  Created with curiosity by Drew Hutton <a
+    class="text-purple-400 hover:text-purple-600"
+    href="https://github.com/yoroshikun/">Yoroshikun</a
+  >. API by
   <a class="text-purple-400 hover:text-purple-600" href="https://pokeapi.co/">PokeAPI</a>
 </div>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,9 +1,9 @@
 <nav class="py-2 md:py-4">
   <div class="container px-4 mx-auto md:flex md:items-center">
     <div class="flex justify-between items-center w-full">
-      <a href="/" class="text-xl font-bold text-purple-400 hover:text-purple-500"> Pokedex Svelte Test </a>
+      <a href="/" class="text-xl font-bold text-purple-400 hover:text-purple-500"> Svelte Pokedex </a>
       <div class="mx-4">
-        <a href="/">
+        <a href="https://github.com/yoroshikun/svelte-pokedex">
           <div class="border border-purple-600 rounded-lg p-2 text-white hover:bg-purple-500/20 cursor-pointer">
             Github
           </div>
